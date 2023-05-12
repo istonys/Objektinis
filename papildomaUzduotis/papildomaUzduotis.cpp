962f4e842e2c799b0a3ab042d7ded75defbc54f6
istonys <116721418+istonys@users.noreply.github.com>
2023-05-12 09:09:29 +0300

    Add files via upload

--- papildomaUzduotis/papildomaUzduotis.cpp
+++ papildomaUzduotis/papildomaUzduotis.cpp
@@ -1,6 +1,3 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include <iostream>
 #include <fstream>
 #include <regex>
@@ -9,6 +6,7 @@
 #include <string>
 #include <algorithm>
 #include <set>
+#include <vector>
 
 using std::cout;
 using std::string;
@@ -16,27 +14,27 @@ using std::ifstream;
 using std::stringstream;
 using std::endl;
 using std::map;
+using std::vector;
 
-void skaitytiIrRasyti(map<string, int>& zodziai);
-//void Rasyti();
+void skaitytiIrRasyti(map<string, int>& zodziai, map<string, vector<int>>& zodziuLines);
 
 int main()
 {
 	map<string, int> zodziai;
-	skaitytiIrRasyti(zodziai);
+	map<string, vector<int>> zodziuLines;
+	skaitytiIrRasyti(zodziai, zodziuLines);
 
 	return 0;
 }
-void skaitytiIrRasyti(map<string, int>& zodziai) {
+void skaitytiIrRasyti(map<string, int>& zodziai, map<string, vector<int>>& zodziuLines) {
 	// ieskome link'u
 	string filename = "tekstas.txt";
-	std::locale::global(std::locale("lt_LT.UTF-8")); // Set the global locale to Lithuanian UTF-8
+	std::locale::global(std::locale("lt_LT.UTF-8"));
 	ifstream file(filename);
 	std::ofstream outfile("output.txt", std::ios_base::out | std::ios_base::binary); // open file in binary mode
-	outfile.imbue(std::locale("lt_LT.UTF-8")); // set the encoding of the file to UTF-8
 	string text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 	file.close();
-	std::regex url_regex(R"((https?://|www\.)\S+(\.lt|\.com)\b)");
+	std::regex url_regex("(?:(http|https)://)?([a-zA-Z0-9\\-.]+\\.(com|org|net|edu|gov|mil|biz|info|io|lt|me|tv))(\\S*)?");
 	std::smatch matches;
 	outfile << "URLs found in the text:" << endl;
 	std::set<string> unique_urls;
@@ -53,19 +51,29 @@ void skaitytiIrRasyti(map<string, int>& zodziai) {
 	// skaiciuojame zodzius
 	stringstream ss(text);
 	string zodis;
+	int lineNum = 1;
 	while (ss >> zodis) {
 		if (!regex_match(zodis, url_regex)) {
 			zodis.erase(std::remove_if(zodis.begin(), zodis.end(), ispunct), zodis.end());
 		}
 		if (!zodis.empty()) {
 			zodziai[zodis]++;
+			zodziuLines[zodis].push_back(lineNum);
 		}
+		lineNum++;
 	}
 	outfile << endl;
 	outfile << "Word counts:" << endl;
 	for (const auto& pair : zodziai) {
+		outfile.imbue(std::locale("lt_LT.UTF-8"));
 		if (pair.second > 1 && !all_of(pair.first.begin(), pair.first.end(), ::isdigit)) {
 			outfile << pair.first << ": " << pair.second << endl;
+			outfile << " Lines: ";
+			outfile.imbue(std::locale("en_US.UTF-8"));
+			for (const auto& lineNum : zodziuLines[pair.first]) {
+				outfile << lineNum << " ";
+			}
+			outfile << endl;
 		}
 	}
 }
